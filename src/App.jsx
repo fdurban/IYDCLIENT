819f75faaafdf6f8cf10a6149ad03c8898e9833b
Fernando Durban <ferbrizie@gmail.com>
2023-05-30 20:18:25 +0200

    cambios fer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
+import { Route, Routes } from 'react-router-dom'
 
-function App() {
+const AppRoutes = () => {
+
   return (
-    <h1>Is your day!</h1>
-  );
+    <Routes>
+      <Route path='/' element={<h1>Inicio</h1>} />
+      <Route path='/cards' element={<h1>Cards</h1>} />
+      <Route path='/cards/categories' element={<h1>Cards-tegories</h1>} />
+      <Route path='/cards/category/:category' element={<h1>Cards</h1>} />
+      <Route path='/cards/create' element={<h1>Create cards</h1>} />
+      <Route path='/profile' element={<h1>Profile</h1>} />
+      <Route path='/signup' element={<h1>Sign up!</h1>} />
+      <Route path='/login' element={<h1>Sign in!</h1>} />
+    </Routes>
+  )
 }
 
-export default App;
+export default AppRoutes
--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import Navigation from './components/Navigation/Navigation'
+
 import { BrowserRouter as Router } from "react-router-dom"
-import Footer from './components/Footer/Footer'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <Navigation />
+
       <App />
-      <Footer />
+
     </Router>
   </React.StrictMode>
 );
