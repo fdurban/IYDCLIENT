84ed437586b59179afb1f41d8c715e6091a289b5
Fernando Durban <ferbrizie@gmail.com>
2023-06-04 20:59:22 +0200

    cambiosfer

--- src/components/SubjectList/SubjectList.jsx
+++ src/components/SubjectList/SubjectList.jsx
@@ -29,17 +29,17 @@ const SubjectList = () => {
     return (
         <>
             <h3>This is your subject list</h3>
-            <Link to={'/cards/subject/maths/:id'}> <strong>Matemati-cards</strong> (Matematics)</Link>
-            <Link to={'/cards/subject/anatomy/:id'}><strong>Card-nathomy</strong> (Anathomy)</Link>
-            <Link to={'/cards/subject/biology/:id'}><strong>Biology</strong></Link>
-            <Link to={'/cards/subject/chemistry/:id'}><strong>Chemistry</strong></Link>
-            <Link to={'/cards/subject/geografy/:id'}><strong>Geo-card-phy</strong> (Geography)</Link>
-            <Link to={'/cards/subject/history/:id'}><strong>History</strong></Link>
-            <Link to={'/cards/subject/law/:id'}><strong>Law</strong></Link>
-            <Link to={'/cards/subject/music/:id'}><strong>Music</strong></Link>
-            <Link to={'/cards/subject/programing/:id'}><strong>Pro-card-ming</strong> (Programing)</Link>
-            <Link to={'/cards/subject/physics/:id'}><strong>Phisics</strong></Link>
-            <Link to={'/cards/subject/others/:id'}><strong>Other</strong></Link>
+            <Link to={'/cards/subject/maths'}> <strong>Matemati-cards</strong> (Matematics)</Link>
+            <Link to={'/cards/subject/anatomy'}><strong>Card-nathomy</strong> (Anathomy)</Link>
+            <Link to={'/cards/subject/biology'}><strong>Biology</strong></Link>
+            <Link to={'/cards/subject/chemistry'}><strong>Chemistry</strong></Link>
+            <Link to={'/cards/subject/geografy'}><strong>Geo-card-phy</strong> (Geography)</Link>
+            <Link to={'/cards/subject/history'}><strong>History</strong></Link>
+            <Link to={'/cards/subject/law'}><strong>Law</strong></Link>
+            <Link to={'/cards/subject/music'}><strong>Music</strong></Link>
+            <Link to={'/cards/subject/programing'}><strong>Pro-card-ming</strong> (Programing)</Link>
+            <Link to={'/cards/subject/physics'}><strong>Phisics</strong></Link>
+            <Link to={'/cards/subject/others'}><strong>Other</strong></Link>
             {/* {cards.map(elm => (
                 <Col md={{ span: 4 }} key={elm.subject}>
                     <CardsComponents {...elm} />
